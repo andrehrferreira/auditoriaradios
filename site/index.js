f0b2f3294c2fbc0e7495dc26d52af357c291e311
Oswaldo de Carvalho Filho <oswaldo.vix@gmail.com>
2022-10-29 00:53:48 -0300

    busca de rádios

--- site/index.js
+++ site/index.js
@@ -11,6 +11,48 @@ $(window).on( "load", async () => {
     $("#queueFiles").html(relatorio.queueFiles.toLocaleString() + " (" + ((relatorio.queueFiles * 15) / 60).toFixed(0) + ` horas de audio` + ")");
 });
 
+let toHandler = null
+async function onSearchKeyUp(text) {
+
+    if (radios == null) {
+        radios = await $.getJSON("/data/radios.json");
+    }
+    
+    if (toHandler!=null) {
+        clearTimeout(toHandler)
+    }
+
+    if (text.length < 3) {
+        $('#mainAccordion').html(`<div class="alert alert-info">Difite pelo menos 3 caracteres</div>`)
+        return
+    }
+
+    $('#mainAccordion').html(`
+        <div class="alert alert-success">Procurando...</div>
+    `)
+
+    toHandler = setTimeout(() => {
+
+        $('#mainAccordion').html('')
+
+        let i = 0
+        text = accentsTidy(text).toLowerCase()
+
+        Object.keys(radios).forEach((uf) => {
+            Object.values(radios[uf]).forEach((radio) => {
+                if (radio.cleanName.includes(text)) {
+                    drawRadio(radio, i++)
+                }
+            });
+        });
+
+        if (i==0) {
+            $('#mainAccordion').html(`<div class="alert alert-warning">Nenhuma rádio encontrada</div>`)
+        }
+
+    }, 1500);
+}
+
 function playRadio(src) {
     
     $('#player').html('')
@@ -27,6 +69,65 @@ function playRadio(src) {
     player.play();
 }
 
+async function drawRadio(radio, i) {
+    $('#mainAccordion').append(`
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-heading${i}">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse${i}" aria-expanded="false" aria-controls="panelsStayOpen-collapse${i}">
+                ${radio.name}
+            </button>
+            </h2>
+            <div id="panelsStayOpen-collapse${i}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading${i}">
+            <div class="accordion-body">
+                <p>Localização: ${radio.location}</p>
+
+                <p class="text-center" style="width: 200px; height: 200px">
+                    <canvas id="chart${i}"></canvas>
+                </p>
+                
+                <div class="d-flex justify-content-between">
+                    
+                    <a href="${radio.site}" title="Website">
+                        <i class="fa-solid fa-link"></i>
+                    </a>
+
+                    <a href="${radio.name} - ${radio.location} - 2022-9-28.txt" target="_blank" title="Transcrição">
+                        <i class="fa-regular fa-file-lines"></i>
+                    </a>
+
+                    <a href="javascript:playRadio('${radio.url}');" title="Ouvir rádio">
+                        <i class="fa-solid fa-music"></i>
+                    </a>
+                </div>
+            </div>
+            </div>
+        </div>
+    `)
+
+    if (radio.insercoes.Lula + radio.insercoes.Bolsonaro > 0) {
+        new Chart(
+            document.getElementById('chart' + i), {
+                type: 'doughnut',
+                data: {
+                    labels: [
+                        'Lula',
+                        'Bolsonaro'
+                    ],
+                    datasets: [{
+                        label: 'Inserções',
+                        data: [radio.insercoes.Lula, radio.insercoes.Bolsonaro],
+                        backgroundColor: [
+                            'rgb(255, 99, 132)',
+                            'rgb(54, 162, 235)'
+                        ],
+                        hoverOffset: 4
+                    }]
+                }
+            }
+        )
+    }
+}
+
 async function selectOne(uf) {
 
     if (radios == null) {
@@ -34,69 +135,31 @@ async function selectOne(uf) {
     }
 
     $('#mainAccordion').html('')
-    let collapsed = ''
     for (let i = 0; i< radios[uf].length; i++) {
 
         let radio = radios[uf][i]
         
-        $('#mainAccordion').append(`
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="panelsStayOpen-heading${i}">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse${i}" aria-expanded="false" aria-controls="panelsStayOpen-collapse${i}">
-                    ${radio.name}
-                </button>
-                </h2>
-                <div id="panelsStayOpen-collapse${i}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading${i}">
-                <div class="accordion-body">
-                    <p>Localização: ${radio.location}</p>
-
-                    <p class="text-center" style="width: 200px; height: 200px">
-                        <canvas id="chart${i}"></canvas>
-                    </p>
-                    
-                    <div class="d-flex justify-content-between">
-                        
-                        <a href="${radio.site}" title="Website">
-                            <i class="fa-solid fa-link"></i>
-                        </a>
-
-                        <a href="${radio.name} - ${radio.location} - 2022-9-28.txt" target="_blank" title="Transcrição">
-                            <i class="fa-regular fa-file-lines"></i>
-                        </a>
-
-                        <a href="javascript:playRadio('${radio.url}');" title="Ouvir rádio">
-                            <i class="fa-solid fa-music"></i>
-                        </a>
-                    </div>
-                </div>
-                </div>
-            </div>
-        `)
-
-        if (radio.insercoes.Lula + radio.insercoes.Bolsonaro > 0) {
-            new Chart(
-                document.getElementById('chart' + i), {
-                    type: 'doughnut',
-                    data: {
-                        labels: [
-                            'Lula',
-                            'Bolsonaro'
-                        ],
-                        datasets: [{
-                            label: 'Inserções',
-                            data: [radio.insercoes.Lula, radio.insercoes.Bolsonaro],
-                            backgroundColor: [
-                                'rgb(255, 99, 132)',
-                                'rgb(54, 162, 235)'
-                            ],
-                            hoverOffset: 4
-                        }]
-                    }
-                }
-            )
-        }
+        drawRadio(radio, i)
     }
 }
+
+//
+function accentsTidy (s) {
+    var r=s.toLowerCase();
+    r = r.replace(new RegExp(/\s/g),"");
+    r = r.replace(new RegExp(/[àáâãäå]/g),"a");
+    r = r.replace(new RegExp(/æ/g),"ae");
+    r = r.replace(new RegExp(/ç/g),"c");
+    r = r.replace(new RegExp(/[èéêë]/g),"e");
+    r = r.replace(new RegExp(/[ìíîï]/g),"i");
+    r = r.replace(new RegExp(/ñ/g),"n");                
+    r = r.replace(new RegExp(/[òóôõö]/g),"o");
+    r = r.replace(new RegExp(/œ/g),"oe");
+    r = r.replace(new RegExp(/[ùúûü]/g),"u");
+    r = r.replace(new RegExp(/[ýÿ]/g),"y");
+    r = r.replace(new RegExp(/\W/g),"");
+    return r;
+  }
 
 $(".map-container").mapael({
     map: {
